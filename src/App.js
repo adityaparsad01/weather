31be81927c497262f1d03e75e5bda622488083c1
unknown <adityaparsad01@gmail.com>
2020-07-18 15:15:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day}${date}${month}${year}`;
+    return `${day} ${date} ${month} ${year}`;
   };
 
   return (
